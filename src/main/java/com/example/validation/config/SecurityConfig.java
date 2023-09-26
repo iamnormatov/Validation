c692fbc1866c66c8d1ee5f6bed9bd7754f46f751
Azizjon <azizjonnormatov437@gmail.com>
2023-09-26 19:36:39 +0500

    Updating some package

--- src/main/java/com/example/validation/config/SecurityConfig.java
+++ src/main/java/com/example/validation/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -44,16 +46,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChainChain(HttpSecurity http) throws Exception {
+        String[] access = {
+                "/user/**",
+                "/swagger-ui/**",
+                "/swagger-resources/*",
+                "*.html",
+                "/api/v1/swagger.json"
+        };
+
         String[] users = {"/user/create", "/user/get"};
         return http
                 .csrf().disable()
                 .cors().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/book/**", "/user/**").permitAll()
+                .requestMatchers(access).permitAll()
                 .anyRequest()
                 .authenticated().and()
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**");
     }
 
 }
